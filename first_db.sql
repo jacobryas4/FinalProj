e3ee9084d4b0107096554b9ca81cbee7eef4e8cd
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-11-10 18:21:53 -0500

    Add files via upload

--- first_db.sql
+++ first_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2018 at 07:39 PM
+-- Generation Time: Nov 11, 2018 at 12:20 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -63,6 +63,27 @@ CREATE TABLE `balance` (
 INSERT INTO `balance` (`balance_id`, `account_id`, `balance_total`, `last_update`) VALUES
 (1, 1, '55.55', '2018-11-09 18:38:08.119572');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transaction_id` int(13) NOT NULL,
+  `balance_id` int(13) NOT NULL,
+  `deposit` float(11,2) DEFAULT NULL,
+  `withdraw` float(11,2) DEFAULT NULL,
+  `date_of_transaction` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transaction_id`, `balance_id`, `deposit`, `withdraw`, `date_of_transaction`) VALUES
+(1, 1, 55.55, NULL, '2018-11-10 22:16:04');
+
 --
 -- Indexes for dumped tables
 --
@@ -72,6 +93,13 @@ INSERT INTO `balance` (`balance_id`, `account_id`, `balance_total`, `last_update
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `account_id` (`balance_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -87,6 +115,11 @@ ALTER TABLE `account`
 --
 ALTER TABLE `balance`
   MODIFY `balance_id` int(13) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
