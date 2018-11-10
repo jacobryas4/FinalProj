aa9887d71fc327efe5604d19b2288a27e0553a5f
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-11-10 16:55:18 -0500

    Add files via upload

--- models/account_model.class.php
+++ models/account_model.class.php
@@ -76,4 +76,56 @@ class AccountModel {
         return $accounts;
     }
 
+    //display an individual account
+    public function view_account($id) {
+
+        //the select sql statement
+        $sql = "SELECT * "
+                . "FROM " . $this->tblAccount .
+                " WHERE " . $this->tblAccount . ".id='$id'";
+
+        //execute the query
+        $query = $this->dbConnection->query($sql);
+
+        if ($query && $query->num_rows > 0) {
+            $obj = $query->fetch_object();
+
+            //create a account object
+            $account = new Account(
+                    stripslashes($obj->account_id), stripslashes($obj->email), stripslashes($obj->username), stripslashes($obj->password));
+
+            //set the id for the account
+            $account->setId($obj->id);
+
+            return $account;
+        }
+
+        return false;
+    }
+
+    //update account method to adjust sample data about a user
+    public function update_account($id) {
+        //check if data was received, end the program if it was not.
+        if (!filter_has_var(INPUT_POST, 'account_id') ||
+                !filter_has_var(INPUT_POST, 'email') ||
+                !filter_has_var(INPUT_POST, 'username') ||
+                !filter_has_var(INPUT_POST, 'password')) {
+
+            return false;
+        }
+
+        //retrieve data for the new account; data are sanitized and escaped for security.
+        $email = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING)));
+        $username = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING)));
+        $password = $this->dbConnection->real_escape_string(filter_input(INPUT_POST, 'password', FILTER_DEFAULT));
+
+        //query string for update 
+        $sql = "UPDATE " . $this->tblAccount .
+                " SET email='$email', username='$username', password='$password'"
+                . "WHERE id='$id'";
+
+        //execute the query
+        return $this->dbConnection->query($sql);
+    }
+
 }
