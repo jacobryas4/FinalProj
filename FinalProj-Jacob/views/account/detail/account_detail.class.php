8255a667b7cf50c62af2aaee4bca6e8c82b4978f
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-11-14 11:04:36 -0500

    Add files via upload

---
+++ FinalProj-Jacob/views/account/detail/account_detail.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Author: Adam Patrick
+ * Date: 11/11/18
+ * File Name: account_detail.class.php
+ * Description: Displays details of the selected account
+ */
+
+class AccountDetail {
+    
+}
