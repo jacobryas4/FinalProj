59710fc5a31ef1def2d93d217b5c35ffcb609250
Jacob Bryant <jacobryas4@gmail.com>
2018-11-20 17:58:19 -0500

    Add files via upload
    admin views, including an admin dashboard.

---
+++ views/admin/admin_index_view.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of admin_index
+ *
+ * @author jacobbryant
+ */
+class AdminIndexView extends IndexView {
+    
+    public static function displayHeader($title){
+        parent::displayHeader($title);
+    }
+    
+    public static function displayFooter(){
+        parent::displayFooter();
+    }
+    
+}
---
+++ views/admin/index/admin_index.class.php
@@ -0,0 +1,59 @@
+<?php
+
+
+/**
+ * This is a dashboard that allows the administrator to see all the accounts currently held at the 
+ * bank.
+ *
+ * @author jacobbryant
+ */
+class AdminIndex extends AdminIndexView {
+    public function display($accounts){
+        parent::displayHeader("List All Accounts");
+        
+        ?>
+
+<div class="jumbotron text-center"><h3>Administrator Dashboard</h3></div>
+            
+<div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+            <table class="table table-striped bg-light">
+                <thead>
+                  <tr>
+                    <th scope="col">Account ID</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+            
+            <?php
+                    
+            foreach($accounts as $account) {
+                echo "<tr>";
+                
+                $account_id = $account->getAccount_Id();
+                $email = $account->getEmail();
+                $username = $account->getUsername();
+                
+                echo "<td>" . $account_id . "</td>";
+                echo "<td>" . $email . "</td>";
+                echo "<td>" . $username . "</td>";
+                
+                echo "</tr>"; 
+            }
+        
+        ?>
+        </tbody>
+        </table>
+    </div>   
+    <div class="col-2"></div>
+</div>
+
+
+        <?php
+        parent::displayFooter();
+    }
+}
