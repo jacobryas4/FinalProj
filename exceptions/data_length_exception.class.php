770f60d610b0d875d38d4abb321d22464f70a78f
AdPatric <adammpatrick@gmail.com>
2018-11-29 12:39:12 -0500

    exceptions files created

---
+++ exceptions/data_length_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: Adam Patrick
+ * Date: 11/29/18
+ * File: data_length_exception.class.php
+ * Description: Handles a data length exception
+ */
+class DataLengthException extends Exception {
+    //No need for any code
+}
---
+++ exceptions/database_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Author: Adam Patrick
+ * Date: 11/29/18
+ * File: database_exception.class.php
+ * Description: An exception class that handles a database exception
+ */
+class DatabaseException extends Exception {
+    //No need for any code
+}
