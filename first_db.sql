e20d87a13b51d2ceba9d8236e612b8463bee60d2
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-12-02 13:02:43 -0500

    Add files via upload

--- first_db.sql
+++ first_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2018 at 09:20 PM
+-- Generation Time: Dec 02, 2018 at 07:02 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -32,21 +32,26 @@ USE `first_db`;
 --
 
 DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account` (
-  `account_id` int(13) NOT NULL,
+CREATE TABLE IF NOT EXISTS `account` (
+  `account_id` int(13) NOT NULL AUTO_INCREMENT,
   `email` varchar(30) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
   `balance` float(11,2) NOT NULL,
-  `role` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `role` int(1) NOT NULL,
+  PRIMARY KEY (`account_id`),
+  UNIQUE KEY `email` (`email`,`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `account`
 --
 
 INSERT INTO `account` (`account_id`, `email`, `username`, `password`, `balance`, `role`) VALUES
-(10, 'php@php.net', 'phpuser', '$2y$10$rpETPxUSPlzZDgOrdLQTA.xZ/PDOK7MB/9iT/ofATSpmUV8wS.S3W', 0.00, 0);
+(10, 'php@php.net', 'phpuser', '$2y$10$rpETPxUSPlzZDgOrdLQTA.xZ/PDOK7MB/9iT/ofATSpmUV8wS.S3W', 0.00, 2),
+(11, 'test@gmail.com', 'testboy', '$2y$10$XsPnNoS1InsA7aGBK0Ryiu3Ceb1kckE6UsWztJ87TzuSkdklrfM1.', 0.00, 1),
+(12, 'php@php.net', '12345', '$2y$10$Fm1sAC/OnE.fS7bUtA9oqO/KoFJGDtEWrURQjZvKV2iqYw55A1bZi', 0.00, 1),
+(13, 'test@testmail.com', 'testing', '$2y$10$HfoQ6OcQ6zOHcsb8bppARemR9YmmhdeOqwtkTZ/kZyNPyyBOL53W6', 0.00, 1);
 
 -- --------------------------------------------------------
 
@@ -55,13 +60,15 @@ INSERT INTO `account` (`account_id`, `email`, `username`, `password`, `balance`,
 --
 
 DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE `transaction` (
-  `transaction_id` int(13) NOT NULL,
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `transaction_id` int(13) NOT NULL AUTO_INCREMENT,
   `account_id` int(13) NOT NULL,
   `amount` float(11,2) NOT NULL,
   `transaction_type` tinyint(1) NOT NULL,
-  `date_of_transaction` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `date_of_transaction` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transaction_id`),
+  KEY `FOREIGN KEY` (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaction`
@@ -69,40 +76,6 @@ CREATE TABLE `transaction` (
 
 INSERT INTO `transaction` (`transaction_id`, `account_id`, `amount`, `transaction_type`, `date_of_transaction`) VALUES
 (7, 10, 15.22, 1, '2018-11-29 17:14:23');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`account_id`),
-  ADD UNIQUE KEY `email` (`email`,`username`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`transaction_id`),
-  ADD KEY `FOREIGN KEY` (`account_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `account_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `transaction_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
