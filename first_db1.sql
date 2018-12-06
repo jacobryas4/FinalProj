f737944c31f5a16f63de89d1cdbb58b491a2796c
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-12-05 19:34:50 -0500

    Add files via upload

--- first_db1.sql
+++ first_db1.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2018 at 09:48 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.8
+-- Generation Time: Dec 06, 2018 at 01:34 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `first_db1`
 --
+DROP DATABASE `first_db1`;
 CREATE DATABASE IF NOT EXISTS `first_db1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `first_db1`;
 
@@ -50,7 +49,8 @@ INSERT INTO `account` (`account_id`, `email`, `username`, `password`, `balance`,
 (12, 'php@php.net', '12345', '$2y$10$Fm1sAC/OnE.fS7bUtA9oqO/KoFJGDtEWrURQjZvKV2iqYw55A1bZi', 0.00, 1),
 (13, 'test@testmail.com', 'testing', '$2y$10$HfoQ6OcQ6zOHcsb8bppARemR9YmmhdeOqwtkTZ/kZyNPyyBOL53W6', 0.00, 1),
 (24, 'user@mail.com', 'user', '$2y$10$Ftn6FDWTqy92AcsOyLxd7O0FuNfeKAE8I3JneHCSVKILWpRFpF15q', 0.00, 1),
-(25, 'user1@gmail.com', 'user1', '$2y$10$KoOUTfRXq98GSifxuKdfP.GSjADZdiYCHiCNaeCTBZqN1N39yn/l.', 0.00, 1);
+(25, 'user1@gmail.com', 'user1', '$2y$10$KoOUTfRXq98GSifxuKdfP.GSjADZdiYCHiCNaeCTBZqN1N39yn/l.', 0.00, 1),
+(26, '1234@gmail.com', '1234', '$2y$10$beHvd6NIROn7iu.EQv7LrupF1XksCoyfgufyjiKF6w8BkJDWL699.', 0.00, 1);
 
 -- --------------------------------------------------------
 
@@ -64,7 +64,7 @@ CREATE TABLE `transaction` (
   `account_id` int(13) NOT NULL,
   `recipient` varchar(255) NOT NULL,
   `amount` float(11,2) NOT NULL,
-  `transaction_type` tinyint(1) NOT NULL,
+  `transaction_type` varchar(255) NOT NULL,
   `date_of_transaction` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -73,8 +73,8 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`transaction_id`, `account_id`, `recipient`, `amount`, `transaction_type`, `date_of_transaction`) VALUES
-(7, 10, 'phpuser', 15.22, 1, '2018-12-05 20:43:31'),
-(8, 24, 'user', 24.00, 1, '2018-12-05 20:43:34');
+(7, 10, 'phpuser', 15.22, 'Deposit', '2018-12-06 00:15:38'),
+(8, 24, 'user', 24.00, 'Deposit', '2018-12-06 00:15:43');
 
 --
 -- Indexes for dumped tables
@@ -102,14 +102,12 @@ ALTER TABLE `transaction`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `account_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
-
+  MODIFY `account_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
   MODIFY `transaction_id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
 --
 -- Constraints for dumped tables
 --
@@ -119,7 +117,6 @@ ALTER TABLE `transaction`
 --
 ALTER TABLE `transaction`
   ADD CONSTRAINT `FOREIGN KEY` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
